c0077d93c4064eedd16b6ecc42dc3521edb017ae
Matt Raible <matt.raible@okta.com>
2019-04-16 11:03:07 -0600

    Updates after QA Review

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @RestController
 @SpringBootApplication
@@ -18,10 +17,11 @@ public class DemoApplication extends SpringBootServletInitializer {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping
     @ResponseBody
     public String currentUserName(Authentication authentication) {
         DefaultOidcUser userDetails = (DefaultOidcUser) authentication.getPrincipal();
         return "Hello, " + userDetails.getFullName();
     }
+
 }
